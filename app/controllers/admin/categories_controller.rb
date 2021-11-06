8fc0e6b593d672f18e55c2567a5805d702427693
René R. Clavijo <reneclavijo@live.com>
2021-11-06 09:07:36 -0500

    agregando estilos al CRUD de categorias

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CategoriesController < ApplicationController
   
   layout 'admin_layout'
-  before_action :set_category, only: [:show, :edit, :update, :delete]
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
   
   def index
     @categories = Category.all
