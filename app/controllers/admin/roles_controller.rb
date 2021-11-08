e9390fd25c53f060b10a9cff770e6a172f447220
René R. Clavijo <reneclavijo@live.com>
2021-11-08 18:49:59 -0500

    agregando la semilla y la lógica de Roles

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -4,6 +4,7 @@ class Admin::RolesController < ApplicationController
   before_action :set_role, only: [:show, :edit, :update, :destroy]
   # GET /roles
   def index
+    @roles = Role.all
   end
 
   # GET /roles/:id
@@ -12,6 +13,7 @@ class Admin::RolesController < ApplicationController
 
   # GET /roles/new
   def new
+    @role = Role.new
   end
 
   # GET /roles/:id/edit
@@ -20,22 +22,38 @@ class Admin::RolesController < ApplicationController
 
   # POST /roles
   def create
+    @role = Role.new(params_role)
+    if @role.save
+      redirect_to admin_roles_path
+    else
+      render :new
+    end
   end
 
   # PUT/PATCH /roles/:id
   def update
+    if @role.update(params_role)
+      redirect_to admin_roles_path
+    else
+      render :edit
+    end
   end
 
   # DELETE /roles/:id
   def destroy
+    @role.destroy
+    redirect_to admin_roles_path
   end
   
   private
   def set_role
+    @role = Role.find(params[:id])
+  rescue
+    flash[:set_role_error] = "Could not find the record #{params[:id]}"
+    redirect_to admin_roles_path
   end
 
   def params_role
+    params.require(:role).permit(:name)
   end
-
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Status.create([
+    { description: 'Accepted' },
+    { description: 'Rejected' },
+    { description: 'Canceled' },
+    { description: 'Delivered' },
+    { description: 'Authorized' }
+])
+puts "💾 Statutes loaded!"
+# Save data for Category
+Category.create([
+    { name: 'Food' },
+    { name: 'Meat' },
+    { name: 'Beer' },
+    { name: 'Clothes' }
+])
+puts "💾 Categories loaded!"
+
+Role.create([
+    { name: 'Admin' },
+    { name: 'Client' }
+])
+puts "💾 Roles loaded!"
+
+User.create([
+    { username: 'admin', password: 'admin', password_confirmation: 'admin', role: Role.find_by(name: 'Admin') }
+])
+puts "💾 Users loaded!"
