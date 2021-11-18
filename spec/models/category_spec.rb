fe0d548ca5f2c2fa9424a9f0ebd95a6c99cf1628
René R. Clavijo <reneclavijo@live.com>
2021-11-17 19:35:16 -0500

    terminando la prueba de category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe Category, type: :model do
   describe '#create' do
       it 'when attributes are not valid' do
         # Arrange | Arreglar  | Preparar
+        new_category = Category.new
         # Act     | Actar     | Ejecutar
+        new_category.save
         # Assert  | Afirmas   | Validar
+        expect(new_category.errors[:name].count).to_not be(0)
       end
   end
 end
