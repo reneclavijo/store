ce973c0354f5ec9ea4619c9ad4711afe6bf7a122
René R. Clavijo <reneclavijo@live.com>
2021-11-17 19:02:04 -0500

    agreagando la base de las pruebas

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  describe '#create' do
+      it 'when attributes are not valid' do
+        # Arrange | Arreglar  | Preparar
+        # Act     | Actar     | Ejecutar
+        # Assert  | Afirmas   | Validar
+      end
+  end
+end
